595b5f3e02ac649d316e083d0837bb9f7cc366e3
Todd Seller <me@toddseller.com>
2016-06-14 08:44:03 -0700

    Add styling to weather page

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,7 +2,7 @@ var React = require('react')
 
 var WeatherMessage = ({location, temp, max}) => {
   return (
-    <h3>It is currently {temp}&deg; in {location} with a high of {max}&deg; today.</h3>
+    <h3 className="text-center">It is currently {temp}&deg; in {location}, with a high of {max}&deg; today.</h3>
   )
 }
 
